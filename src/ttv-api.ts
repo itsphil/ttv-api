407332947224c7785a80541b5ab9c4ecadfd9a93
Phil Zeelte <phil.zeelte@setl.io>
2019-08-19 15:45:45 +0100

    added initial methods to ttvapi

--- src/ttv-api.ts
+++ src/ttv-api.ts
@@ -1,1 +1,72 @@
-export class TTVAPI { }
+export class TTVAPI {
+    constructor(private bearer: string) {}
+
+    getExtensionAnalytics() {}
+
+    getGameAnalytics() {}
+
+    getBitsLeaderboard() {}
+
+    getExtensionTransactions() {}
+
+    createClip() {}
+
+    getClips() {}
+
+    createEntitlementGrantsUploadURL() {}
+
+    getCodeStatus() {}
+
+    redeemCode() {}
+
+    getTopGames() {}
+
+    getGames() {}
+
+    checkAutomodStatus() {}
+
+    getBannedEvents() {}
+
+    getBannedUsers() {}
+
+    getModerators() {}
+
+    getModeratorEvents() {}
+
+    getStreams() {}
+
+    getStreamsMetadata() {}
+
+    createStreamMarker() {}
+
+    getStreamMarkers() {}
+
+    getBroadcasterSubscriptions() {}
+
+    getBroadcasterSubscribers() {}
+
+    getSubscriptionEvents() {}
+
+    getAllStreamTags() {}
+
+    getStreamTags() {}
+
+    replaceStreamTags() {}
+
+    getUsers() {}
+
+    getUsersFollows() {}
+
+    updateUser() {}
+
+    getUserExtensions() {}
+
+    getUserActiveExtensions() {}
+
+    updateUserExtensions() {}
+
+    getVideos() {}
+
+    getWebhookSubscriptions() {}
+
+}
